4a711c34d273587d2ee7480356b907bfa2236646
progman <gnuplanet@gmail.com>
2021-01-13 21:03:18 +0300

    add usetest.go

---
+++ usetest.go
@@ -0,0 +1,48 @@
+/*
+package main
+
+import "github.com/progman/libcore.go"
+import "fmt"
+
+
+func zzz(val int) libcore.Result {
+	var result libcore.Result
+	result.Init()
+
+	if val == 1 {
+		result.SetErrMsg(1, "fucked up situation")
+	}
+
+	if val == 2 {
+		result.SetOk()
+	}
+
+	return result
+}
+
+
+func main() {
+	var rc libcore.Result
+
+	rc = zzz(0)
+	if rc.IsOk() == false {
+		fmt.Printf("%s\n", rc.Draw1())
+	}
+
+	rc = zzz(1)
+	if rc.IsOk() == false {
+		fmt.Printf("%s\n", rc.Draw2())
+	}
+
+	rc = zzz(1)
+	if rc.IsOk() == false {
+		fmt.Printf("%s\n", rc.Draw3())
+	}
+
+	rc = zzz(2)
+	fmt.Printf("%s\n", rc.Draw3())
+
+	fmt.Printf("%s\n", libcore.Ping())
+	fmt.Printf("%s\n", libcore.Pong())
+}
+*/
