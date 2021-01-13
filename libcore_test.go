2600215edfe341a3fc7a7f65fd129033d2a0391e
progman <gnuplanet@gmail.com>
2021-01-13 20:07:34 +0300

    update

--- libcore_test.go
+++ libcore_test.go
@@ -4,7 +4,8 @@ import "testing"
 
 func TestHello(t *testing.T) {
 	want := "Hello, world."
-	if got := Hello(); got != want {
+	got := Hello();
+	if got != want {
 		t.Errorf("Hello() = %q, want %q", got, want)
 	}
 }
