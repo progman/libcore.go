3a182fcd89e516168088f568ef361747f9258ec1
progman <gnuplanet@gmail.com>
2024-01-08 22:58:16 +0600

    rewrite UnixnanotimeUnpack(), rename Unixnanotime2iso() to UnixnanotimeToIso(), rename Unixnanotime_str2iso() to UnixnanotimeStrToIso(), rename Unixmicrotime2iso() to UnixmicrotimeToIso(), rename Unixmicrotime_str2iso() to UnixmicrotimeStrToIso(), rename Unixtime2iso() to UnixtimeToIso(), rename Unixtime_str2iso() to UnixtimeStrToIso()

--- Isotime.go
+++ Isotime.go
@@ -24,32 +24,32 @@ func GetUnixnanotimeUtcInt64() (unixnanotimeUtc int64) {
 }
 //-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
 func GetUnixnanotimeUtc() (unixnanotimeUtc string) {
-	unixnanotimeUtc = fmt.Sprintf("%d", GetUnixnanotimeUtcInt64())
+	unixnanotimeUtc = SintToStr(GetUnixnanotimeUtcInt64())
 	return
 }
 //-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
 func GetUnixmicrotimeUtcInt64() (unixmicrotimeUtc int64) {
-	unixmicrotimeUtc = time.Now().UTC().UnixNano() / int64(time.Microsecond)
+	unixmicrotimeUtc = GetUnixnanotimeUtcInt64() / int64(time.Microsecond)
 	return
 }
 //-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
 func GetUnixmicrotimeUtc() (unixmicrotimeUtc string) {
-	unixmicrotimeUtc = fmt.Sprintf("%d", GetUnixmicrotimeUtcInt64())
+	unixmicrotimeUtc = SintToStr(GetUnixmicrotimeUtcInt64())
 	return
 }
 //-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
-func UnixnanotimeUnpack(unixnanotime int64) (target Time) {
+func UnixnanotimeUnpack(unixnanotime int64, pTime *Time) {
 	t := time.Unix(0, unixnanotime)
 	t = t.UTC()
 
-	target.Year       = t.Year()
-	target.Month      = int(t.Month())
-	target.Day        = t.Day()
-	target.Hour       = t.Hour()
-	target.Minute     = t.Minute()
-	target.Second     = t.Second()
-	target.Nanosecond = t.Nanosecond()
-	target.WeekDay    = int(t.Weekday())
+	pTime.Year       = t.Year()
+	pTime.Month      = int(t.Month())
+	pTime.Day        = t.Day()
+	pTime.Hour       = t.Hour()
+	pTime.Minute     = t.Minute()
+	pTime.Second     = t.Second()
+	pTime.Nanosecond = t.Nanosecond()
+	pTime.WeekDay    = int(t.Weekday())
 
 	return
 }
@@ -63,63 +63,73 @@ func UnixnanotimePack(pTime *Time) (unixnanotime int64) {
 }
 //-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
 // 1624120658256315911 -> "2021-06-19 16:37:38.256315911"
-func Unixnanotime2iso(unixnanotime int64) (iso string) {
+// Obsolete: func Unixnanotime2iso(unixnanotime int64) (iso string) {
+func UnixnanotimeToIso(unixnanotime int64, offset int64, flagNanoseconds bool) (iso string) {
 	var t1 Time
 
-	t1 = UnixnanotimeUnpack(unixnanotime)
+	UnixnanotimeUnpack(unixnanotime + offset, &t1)
 
-	iso = fmt.Sprintf("%04d-%02d-%02d %02d:%02d:%02d.%09d", t1.Year, t1.Month, t1.Day, t1.Hour, t1.Minute, t1.Second, t1.Nanosecond)
+	if flagNanoseconds == false {
+		iso = fmt.Sprintf("%04d-%02d-%02d %02d:%02d:%02d",      t1.Year, t1.Month, t1.Day, t1.Hour, t1.Minute, t1.Second)
+	} else {
+		iso = fmt.Sprintf("%04d-%02d-%02d %02d:%02d:%02d.%09d", t1.Year, t1.Month, t1.Day, t1.Hour, t1.Minute, t1.Second, t1.Nanosecond)
+	}
 
 	return
 }
 //-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
 // "1624120658256315911" -> "2021-06-19 16:37:38.256315911"
-func Unixnanotime_str2iso(unixnanotime_str string) (iso string) {
+// Obsolete: func Unixnanotime_str2iso(unixnanotime_str string) (iso string) {
+func UnixnanotimeStrToIso(unixnanotimeStr string, offset int64, flagNanoseconds bool) (iso string) {
+	var err error
+	var unixnanotime int64
 
-	var unixnanotime int64 = 0
-
-	tmp, err := strconv.ParseUint(unixnanotime_str, 10, 64)
-	if err == nil {
-		unixnanotime = int64(tmp)
+	unixnanotime, err = StrToSint(unixnanotimeStr)
+	if err != nil {
+		unixnanotime = 0
 	}
 
-	return Unixnanotime2iso(unixnanotime)
+	return UnixnanotimeToIso(unixnanotime, offset, flagNanoseconds)
 }
 //-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
 // 1624120658256315 -> "2021-06-19 16:37:38.256315000"
-func Unixmicrotime2iso(unixmicrotime int64) (iso string) {
-	return Unixnanotime2iso(unixmicrotime * 1000)
+// Obsolete: func Unixmicrotime2iso(unixmicrotime int64) (iso string) {
+func UnixmicrotimeToIso(unixmicrotime int64, offset int64, flagNanoseconds bool) (iso string) {
+	return UnixnanotimeToIso(unixmicrotime * 1000, offset, flagNanoseconds)
 }
 //-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
 // "1624120658256315" -> "2021-06-19 16:37:38.256315000"
-func Unixmicrotime_str2iso(unixmicrotime_str string) (iso string) {
+// Obsolete: func Unixmicrotime_str2iso(unixmicrotime_str string) (iso string) {
+func UnixmicrotimeStrToIso(unixmicrotimeStr string, offset int64, flagNanoseconds bool) (iso string) {
+	var err error
+	var unixmicrotime int64
 
-	var unixmicrotime int64 = 0
-
-	tmp, err := strconv.ParseUint(unixmicrotime_str, 10, 64)
-	if err == nil {
-		unixmicrotime = int64(tmp)
+	unixmicrotime, err = StrToSint(unixmicrotimeStr)
+	if err != nil {
+		unixmicrotime = 0
 	}
 
-	return Unixnanotime2iso(unixmicrotime * 1000)
+	return UnixnanotimeToIso(unixmicrotime * 1000, offset, flagNanoseconds)
 }
 //-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
 // 1624120658 -> "2021-06-19 16:37:38.000000000"
-func Unixtime2iso(unixtime int64) (iso string) {
-	return Unixnanotime2iso(unixtime * 1000000000)
+// Obsolete: func Unixtime2iso(unixtime int64) (iso string) {
+func UnixtimeToIso(unixtime int64, offset int64, flagNanoseconds bool) (iso string) {
+	return UnixnanotimeToIso(unixtime * 1000000000, offset, flagNanoseconds)
 }
 //-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
 // "1624120658" -> "2021-06-19 16:37:38.000000000"
-func Unixtime_str2iso(unixtime_str string) (iso string) {
+// Obsolete: func Unixtime_str2iso(unixtime_str string) (iso string) {
+func UnixtimeStrToIso(unixtimeStr string, offset int64, flagNanoseconds bool) (iso string) {
+	var err error
+	var unixtime int64
 
-	var unixtime int64 = 0
-
-	tmp, err := strconv.ParseUint(unixtime_str, 10, 64)
-	if err == nil {
-		unixtime = int64(tmp)
+	unixtime, err = StrToSint(unixtimeStr)
+	if err != nil {
+		unixtime = 0
 	}
 
-	return Unixnanotime2iso(unixtime * 1000000000)
+	return UnixnanotimeToIso(unixtime * 1000000000, offset, flagNanoseconds)
 }
 //-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
 func TimeCropPerDay(source time.Time) (target time.Time) {
@@ -141,7 +151,7 @@ func TimeCropPerDay(source time.Time) (target time.Time) {
 func UnixnanotimeCropPerDay(unixnanotimeSource int64) (unixnanotimeTarget int64) {
 
 	var t1 Time
-	t1 = UnixnanotimeUnpack(unixnanotimeSource)
+	UnixnanotimeUnpack(unixnanotimeSource, &t1)
 
 	t1.Hour       = 0
 	t1.Minute     = 0
@@ -156,7 +166,7 @@ func UnixnanotimeCropPerDay(unixnanotimeSource int64) (unixnanotimeTarget int64)
 func isotime_test() bool {
 	var t1 Time
 
-	t1 = UnixnanotimeUnpack(1624120658256315911)
+	UnixnanotimeUnpack(1624120658256315911, &t1)
 	if (t1.Year != 2021) || (t1.Month != 06) || (t1.Day != 19) || (t1.Hour != 16) || (t1.Minute != 37) || (t1.Second != 38) || (t1.Nanosecond != 256315911) {
 		fmt.Printf("step001\n")
 	}
@@ -171,37 +181,37 @@ func isotime_test() bool {
 		return false
 	}
 
-	if Unixnanotime2iso(UnixnanotimeCropPerDay(1624120658256315911)) != "2021-06-19 00:00:00.000000000" {
+	if UnixnanotimeToIso(UnixnanotimeCropPerDay(1624120658256315911), 0, true) != "2021-06-19 00:00:00.000000000" {
 		fmt.Printf("step004\n")
 		return false
 	}
 
-	if Unixnanotime2iso(1624120658256315911) != "2021-06-19 16:37:38.256315911" {
+	if UnixnanotimeToIso(1624120658256315911, 0, true) != "2021-06-19 16:37:38.256315911" {
 		fmt.Printf("step005\n")
 		return false
 	}
 
-	if Unixnanotime_str2iso("1624120658256315911") != "2021-06-19 16:37:38.256315911" {
+	if UnixnanotimeStrToIso("1624120658256315911", 0, true) != "2021-06-19 16:37:38.256315911" {
 		fmt.Printf("step006\n")
 		return false
 	}
 
-	if Unixmicrotime2iso(1624120658256315) != "2021-06-19 16:37:38.256315000" {
+	if UnixmicrotimeToIso(1624120658256315, 0, true) != "2021-06-19 16:37:38.256315000" {
 		fmt.Printf("step007\n")
 		return false
 	}
 
-	if Unixmicrotime_str2iso("1624120658256315") != "2021-06-19 16:37:38.256315000" {
+	if UnixmicrotimeStrToIso("1624120658256315", 0, true) != "2021-06-19 16:37:38.256315000" {
 		fmt.Printf("step008\n")
 		return false
 	}
 
-	if Unixtime2iso(1624120658) != "2021-06-19 16:37:38.000000000" {
+	if UnixtimeToIso(1624120658, 0, true) != "2021-06-19 16:37:38.000000000" {
 		fmt.Printf("step009\n")
 		return false
 	}
 
-	if Unixtime_str2iso("1624120658") != "2021-06-19 16:37:38.000000000" {
+	if UnixtimeStrToIso("1624120658", 0, true) != "2021-06-19 16:37:38.000000000" {
 		fmt.Printf("step010\n")
 		return false
 	}
@@ -210,13 +220,9 @@ func isotime_test() bool {
 	return true
 }
 //-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
-
-//-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
 /*
 func main() {
 	isotime_test()
-
-
 }
 */
 //-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------//
